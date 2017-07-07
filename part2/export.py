8b287c17dd5e583a0895a9e5ac0afd9e6f71c530
Daniel Farkas <w.farkasdaniel@gmail.com>
2017-07-07 16:39:26 +0200

    most played done

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,12 @@
 
-# Export functions
+def get_most_played(file_name):
+    with open(file_name, mode='r') as txt_file:
+        text = txt_file.readlines()
+        most_played_games = {}
+        for line in text:
+            words = line.split("\t")
+            most_played_games[(float(words[1]))] = words[0]
+
+        sorted_games = sorted(most_played_games.items(), key=lambda x: float(x[0]), reverse=True)
+        return sorted_games[0][1]
+
--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,23 @@
+def main():
+    # print(get_most_played("game_stat.txt"))
+    print(sum_sold("game_stat.txt"))
 
-# Printing functions
+
+# 1test
+def get_most_played(file_name):
+    with open(file_name, mode='r') as txt_file:
+        text = txt_file.readlines()
+        most_played_games = {}
+        for line in text:
+            words = line.split("\t")
+            most_played_games[(float(words[1]))] = words[0]
+
+        sorted_games = sorted(most_played_games.items(), key=lambda x: float(x[0]), reverse=True)
+        return sorted_games[0][1]
+
+
+# 2test
+def sum_sold(file_name):
+    pass
+
+main()
